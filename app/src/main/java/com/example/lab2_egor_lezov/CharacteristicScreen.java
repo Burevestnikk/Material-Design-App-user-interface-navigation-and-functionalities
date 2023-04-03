e7e952acd399a3b2bfc77826ee9d6bd815aa3b7f
Egor Lezov aka Burevestnik <59848803+Burevestnikk@users.noreply.github.com>
2023-04-03 23:14:41 +0200

    upd

--- app/src/main/java/com/example/lab2_egor_lezov/CharacteristicScreen.java
+++ app/src/main/java/com/example/lab2_egor_lezov/CharacteristicScreen.java
@@ -8,31 +8,38 @@ import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.MediaController;
 import android.widget.Toast;
+import android.widget.VideoView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.NotificationCompat;
 import androidx.core.app.NotificationManagerCompat;
 
-import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.views.YouTubePlayerView;
-
 public class CharacteristicScreen extends AppCompatActivity {
-
-    private YouTubePlayerView youTubePlayerView;
     private boolean isConnected() {
         ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
         NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
         return activeNetwork != null && activeNetwork.isConnected();
     }
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.characteristic_screen);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_USER_PORTRAIT);
-        youTubePlayerView = findViewById(R.id.videoView);
+        VideoView videoView = findViewById(R.id.videoView);
+        String videoUrl = "https://abhiandroid.com/ui/wp-content/uploads/2016/04/videoviewtestingvideo.mp4";
+        videoView.setVideoURI(Uri.parse(videoUrl));
+        MediaController mediaController = new MediaController(this);
+        mediaController.setAnchorView(videoView);
+        videoView.setMediaController(mediaController);
+        videoView.start();
 
         if (!isConnected()) {
             startActivity(new Intent(CharacteristicScreen.this, ErrorMessagesInternet.class));
